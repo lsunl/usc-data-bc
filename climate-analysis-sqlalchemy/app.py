8ee054d96e16db608dd1fa6d3ed7b642d5c5d17d
Laura Sun <sun.laura@outlook.com>
2019-03-30 17:17:52 -0700

    edit

--- climate-analysis-sqlalchemy/app.py
+++ climate-analysis-sqlalchemy/app.py
@@ -12,7 +12,7 @@ from flask import Flask, jsonify
 
 #set up database
 #create engine to connect
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite", connect_args={'check_same_thread': False})
 
 #reflect databases and tables
 Base = automap_base()
@@ -47,14 +47,14 @@ oneyeardata = session.query(Measurement.date, Measurement.prcp).filter(Measureme
 #create app routes
 @app.route("/")
 def welcome():
-    return (
-        f"Available routes: <br/>"
-        f"/api/v1.0/precipitation <br/>"
-        f"/api/v1.0/stations <br/>"
-        f"/api/v1.0/tobs <br/>"
-        f"/api/v1.0/start <br/>"
-        f"/api/v1.0/start/end"
-    )
+    return """
+        <h1>Available routes</h1>
+        <a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a><br/>
+        <a href="/api/v1.0/stations">/api/v1.0/stations</a><br/>
+        <a href="/api/v1.0/tobs">/api/v1.0/tobs</a> <br/>
+        <a href="/api/v1.0/2017-01-20">/api/v1.0/2017-01-20</a> <br/>
+        <a href="/api/v1.0/2012-01-01/2012-01-31">/api/v1.0/2012-01-01/2012-01-31</a>
+    """
 
 @app.route("/api/v1.0/precipitation")
 def about():
@@ -69,9 +69,28 @@ def about():
 @app.route("/api/v1.0/stations")
 def station():
 #Return a JSON list of stations from the dataset.
-    allstations = session.query(Station.name).all()
-    all_names = list(np.ravel(allstations))
-    return jsonify(all_names)
+    allstations = session.query(Station.id, Station.name, Station.station, Station.latitude, Station.longitude, Station.elevation).all()
+    formatted_data = []
+    # for s in allstations:
+    #     formatted_data.append({
+    #         "id": s[0],
+    #         "name": s[1],
+    #         "station": s[2],
+    #         "latitude": s[3],
+    #         "longitude": s[4],
+    #         "elevation": s[5]
+    #     })
+    formatted_data = [{
+            "id": s[0],
+            "name": s[1],
+            "station": s[2],
+            "latitude": s[3],
+            "longitude": s[4],
+            "elevation": s[5]
+        } for s in allstations]
+
+
+    return jsonify(formatted_data)
 
 @app.route("/api/v1.0/tobs")
 def tob():
@@ -90,7 +109,7 @@ def description(start):
 #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 #When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
 #When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
-    start_date = dt.datetime.strptime(start, '%y-%m-%d')
+    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
     startonly = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start_date).all()
     tempdata = []
@@ -104,8 +123,8 @@ def description(start):
 
 @app.route("/api/v1.0/<start>/<end>")
 def description2(start,end):
-    start_date= dt.datetime.strptime(start, '%y-%m-%d')
-    end_date= dt.datetime.strptime(end, '%y-%m-%d')
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date= dt.datetime.strptime(end, '%Y-%m-%d')
     desc = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
     temp_data = []
