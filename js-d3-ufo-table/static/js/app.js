c798b131310cd42b5067bb1855728b4fcc3861a4
Laura Sun <sun.laura@outlook.com>
2019-05-09 05:00:51 -0700

    javascript

--- js-d3-ufo-table/static/js/app.js
+++ js-d3-ufo-table/static/js/app.js
@@ -2,45 +2,63 @@
 var tableData = data;
 
 
-// use d3 to select table body
+// use d3 for selections
 var tbody = d3.select("tbody");
 var filterButton = d3.select("#filter-btn");
 var allButton = d3.select("#all-btn");
-// append each row of data to table
 
-allButton.on("click", function(){
+
+// append each row of data to create table
+tableData.forEach(ufo => {
+  console.log(ufo);
+  var row = tbody.append("tr");
+  Object.entries(ufo).forEach(([key, value]) => {
+    console.log(key, value);
+    var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// button to display all data
+allButton.on("click", () => {
   d3.event.preventDefault();
-  tableData.forEach(function(ufo){
+  tableData.forEach(ufo => {
     console.log(ufo);
     var row = tbody.append("tr");
 
-    Object.entries(ufo).forEach(function([key, value]) {
-      console.log(key, value);
-      var cell = tbody.append("td");
-        cell.text(value);
+  Object.entries(ufo).forEach(([key, value]) => {
+    console.log(key, value);
+    var cell = tbody.append("td");
+      cell.text(value);
     });
   });
 });
 
 
-// add an event listener that will display rows that match the input data when button is clicked
-
-filterButton.on("click", function(){
+// event listener to filter data by date
+filterButton.on("click", () => {
 
   d3.event.preventDefault();
+
+  var row = d3.select("tbody").selectAll("td");
+  row.remove();
+
   var inputDate = d3.select("#datetime");
   var inputValue = inputDate.property("value");
+  console.log(inputValue);
 
-  tableData.forEach(function(ufo){
+  var filteredData = tableData.filter( tableData => tableData.datetime === inputValue);
+  console.log(filteredData)
+
+  filteredData.forEach(ufo => {
     console.log(ufo);
     var row = tbody.append("tr");
 
-    Object.entries(ufo).forEach(function([key, value]) {
-      if (value === inputValue){
-        console.log(key, value);
-        var cell = tbody.append("td");
-          cell.text(value);
-    };
-  });
+  Object.entries(ufo).forEach(([key, value]) => {
+    console.log(key, value);
+    var cell = tbody.append("td");
+      cell.text(value);
+});
 });
 });
